843a8dc5f64b28e229efc60d5dd4429b24f245d6
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-02-14 08:26:44 -0400

    Change base path for Vite configuration

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Importante: barra al inicio y final
+  base: '/CineNext/',
   server: {
     port: 5173,
     open: true
