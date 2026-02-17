038423a69ee2d781ae6a1ecef34ee4679c3bffa5
SpAy2024 <alexvillanuevasoto@gmail.com>
2026-02-16 23:49:15 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+
+import { HashRouter } from 'react-router-dom';  // ← Cambio a HashRouter
 import App from './App';
 import './index.css';
 
